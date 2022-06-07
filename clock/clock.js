8cb907a970fd479afdedba978c19ac5d8f32cf06
Danish Shafi <danish.shafi@live.co.uk>
2022-06-07 23:24:19 +0100

    Comments added

--- clock/clock.js
+++ clock/clock.js
@@ -1,17 +1,22 @@
 const displayTime = () => {
+  // Setting GMT Time Zone
   const dateTime = new Date();
   let hours = dateTime.getHours();
   let minutes = dateTime.getMinutes();
   let seconds = dateTime.getSeconds();
 
+  // Setting AM/PM setting dependent on the hour.
   hours > 12 ? session = 'PM' : 'AM';
 
+  // Adding an extra '0' as a prefix to the number if num less than 10.
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
   seconds = (seconds < 10) ? "0" + seconds : seconds;
 
+  // Setting the hardcoded style of how the time will be displayed.
   const time = `${hours} : ${minutes} : ${seconds} ${session}`;
 
+  // Rendering the time variable into the #clock span.
   document.getElementById('clock').innerHTML = time;
 }
 
